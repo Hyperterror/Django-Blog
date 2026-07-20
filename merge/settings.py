407987a52ab04113885d20b209f082cd1379a7ce
Ishit-Agarwal <agarwalishit2006@gmail.com>
2026-07-20 12:19:56 +0530

    changes for pythonanywhere

--- merge/settings.py
+++ merge/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-$5x-@76qaey(!aavwid91x*)t(x(^1feb%6*$4^d7p8igq=1@6'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
